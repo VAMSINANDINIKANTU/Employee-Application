019c8a9813a31e8d4beb9376ea2ac44279f10399
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-05-08 12:14:27 +0530

    update commit

--- src/main/java/com/employeeapp/service/EmployerService.java
+++ src/main/java/com/employeeapp/service/EmployerService.java
@@ -2,10 +2,8 @@ package com.employeeapp.service;
 import java.util.List;
 import java.util.Optional;
 import com.employeeapp.entities.Employer;
-
 public interface EmployerService {
 	public List<Employer> getAll();
 	public Optional<Employer> findEmployerById(Long employerId);
 	public Optional<Employer> findEmployerByName(String employerName);
-
 }
