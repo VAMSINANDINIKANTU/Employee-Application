a90e0843c3c1942724856d3028a24deee2bf11ad
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-05-08 12:10:07 +0530

    update commit

--- src/main/java/com/employeeapp/exceptionhandler/CustomExceptionHandlerController.java
+++ src/main/java/com/employeeapp/exceptionhandler/CustomExceptionHandlerController.java
@@ -2,7 +2,6 @@ package com.employeeapp.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
- 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,18 +15,17 @@ public class CustomExceptionHandlerController {
 	
     @ExceptionHandler(EmployeeNotFoundException.class) 
     public ResponseEntity<ErrorResponse> handleEmployeeNotFoundException(EmployeeNotFoundException ex,WebRequest request){ 
-		 List<String> errors = new ArrayList<>();
-		 errors.add(ex.getLocalizedMessage());
+	     List<String> errors = new ArrayList<>();
+	     errors.add(ex.getLocalizedMessage());
 	     ErrorResponse response=new ErrorResponse(HttpStatus.NOT_FOUND,LocalDateTime.now(),"record not found",errors,request.getDescription(false));
 	     return new ResponseEntity<ErrorResponse>(response,HttpStatus.NOT_FOUND);
-	}
+    }
     
     @ExceptionHandler(EmployerNotFoundException.class) 
     public ResponseEntity<ErrorResponse> handleEmployerNotFoundException(EmployerNotFoundException ex,WebRequest request){ 
-		 List<String> errors = new ArrayList<>();
-		 errors.add(ex.getLocalizedMessage());
+	     List<String> errors = new ArrayList<>();
+	     errors.add(ex.getLocalizedMessage());
 	     ErrorResponse response=new ErrorResponse(HttpStatus.NOT_FOUND,LocalDateTime.now(),"record not found",errors,request.getDescription(false));
 	     return new ResponseEntity<ErrorResponse>(response,HttpStatus.NOT_FOUND);
 	}
-    
 }
